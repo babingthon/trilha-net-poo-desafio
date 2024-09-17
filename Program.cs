64f07abce8c986b35e2d46c649eb11c9a24c5ed5
babingthon <babingthon.bandeira@gmail.com>
2024-09-17 20:40:27 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123123", modelo: "123123", iMEI: "12321321", memoria: 112);
+nokia.Ligar();
+nokia.InstalarAplicativo("X");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "123123", modelo: "123123", iMEI: "12321321", memoria: 112);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tinder");
